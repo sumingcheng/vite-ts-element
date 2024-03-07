dcb558de47a3617bd86a850927cdf473493010f0
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-03-07 11:00:40 +0800

    fix: 增加默认源

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  proxy: {
+    // 配置代理，这对于 API 请求转发到后端服务器特别有用
+    '/api': 'http://localhost:3000',
+  },
+  build: {
+    minify: 'esbuild'
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: 'types/auto-imports.d.ts',
+    }),
+    Components({
+      dts: 'types/components.d.ts',
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  esbuild: {
+    target: 'esnext'
+  }
 })
