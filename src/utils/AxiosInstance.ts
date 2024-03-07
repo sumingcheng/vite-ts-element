6273788a932b341564e3f751c1b0fd632d58f38d
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-03-07 17:37:30 +0800

    fix: 增加规则

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -24,13 +24,17 @@ axiosInstance.interceptors.request.use(
 // 响应拦截器
 axiosInstance.interceptors.response.use(
   (response) => {
-    if (!response.data)
-      throw new Error('无效的响应数据')
+    if (!response.data) {
+      ElMessage.warning('请求失败，请稍后重试')
+      console.log('请求失败，请稍后重试')
+    }
+
     return response.data
   },
   (error: AxiosError) => {
-    if (error.code === 'ECONNABORTED')
+    if (error.code === 'ECONNABORTED') {
       ElMessage.warning('请求超时，请稍后重试')
+    }
 
     // 其他错误处理
     return Promise.reject(error)
