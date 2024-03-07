3b5ca3a980d1144d9d28e7af757033bd7f1012a9
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-03-07 15:17:33 +0800

    eslint: 增加限制

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
 // eslint.config.js
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu(
+  {
+    ignores: ['.DS_Store', '*.d.ts'],
+    rules: {
+      'curly': ['error', 'all'],
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'vue/custom-event-name-casing': ['error', 'kebab-case'],
+      'vue/max-attributes-per-line': ['error', { singleline: 5, multiline: 1 }],
+    },
+  },
+)
