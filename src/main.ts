359f07295f4b2bdd66d4b13d49f765b5a4d5eef7
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-03-07 11:01:38 +0800

    fix: 简化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from '@/router';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import i18n from "@/locales";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.use(i18n)
+app.mount('#root')
