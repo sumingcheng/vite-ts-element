c8122daa1a1f045c930d131f5cbe92e6f3d1d79d
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-03-18 10:27:06 +0800

    fix: 修改类型和文件

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,11 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+// src/vue-shim.d.ts
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  // 假设没有特定的 props 和 emits，这里使用 unknown
+  const component: DefineComponent<unknown, unknown, any>
+  export default component
+}
